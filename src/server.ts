a109259420cf2f3b259ad890c1a6c7b344b7c4ca
Ríad Oliveira <riad.oliveira@hotmail.com>
2020-11-11 02:36:01 -0300

    Cors installation

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 import routes from './routes/index';
 import './database';
 import 'reflect-metadata';
@@ -9,6 +10,7 @@ import GlobalErrorHandler from './errors/GlobalErrorHandler';
 const app = express();
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
+app.use(cors());
 app.use(routes);
 app.use(GlobalErrorHandler);
 
